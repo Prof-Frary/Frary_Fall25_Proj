dc9ea8f0792abd799a551d718777f59dfa2af17c
Prof-Frary <bfrary@qcc.cuny.edu>
2025-10-08 11:50:45 -0400

    ICA 4

--- Frary_Fall25_Proj/Form1.cs
+++ Frary_Fall25_Proj/Form1.cs
@@ -9,7 +9,15 @@ namespace Frary_Fall25_Proj
 
         private void btnQuit_Click(object sender, EventArgs e)
         {
-            this.Close();
+            DialogResult buttonSelected = MessageBox.Show(
+                "Are you sure you want quit?",
+                 "Quitting....",
+                 MessageBoxButtons.YesNo,
+                 MessageBoxIcon.Question);
+            if (buttonSelected == DialogResult.Yes)
+            {
+                this.Close();
+            }
         }
 
         private void btnReset_Click(object sender, EventArgs e)
@@ -18,7 +26,7 @@ namespace Frary_Fall25_Proj
             txtNumWidgets.Clear();
             lstOut.Items.Clear();
             txtCustomerName.Focus();
-            txtTaxRate.Clear(); 
+            txtTaxRate.Clear();
         }
 
         private void btnCalculation_Click(object sender, EventArgs e)
@@ -34,31 +42,57 @@ namespace Frary_Fall25_Proj
             // Input
             // Read from the textbox into the variable
             customerName = txtCustomerName.Text;
+            bool widgetsWorked, rateValid;
             // convert an string to an int
-            numWidgets = int.Parse(txtNumWidgets.Text);
+            //  numWidgets = int.Parse(txtNumWidgets.Text);
+
             //convert a string to a demimal
             // since we want the user to enter as a percent we will need to divide by 100
             // to get it in the correct format to use mathematically 
-            taxRate = decimal.Parse(txtTaxRate.Text)/100.0M;
+           // taxRate = decimal.Parse(txtTaxRate.Text) / 100.0M;
+            // Change Parse to TryParse 
+            // 1 - Change method name from Parse to TryParse
+            // 2 - Move Variable to be second Parameter and add out
+            // 3 - add boolean vairable on left side of =
+            widgetsWorked  = int.TryParse(txtNumWidgets.Text, out numWidgets);
+            rateValid = decimal.TryParse(txtTaxRate.Text, out taxRate);
 
-            //Processing            
-            subTotal = widgetPrice + numWidgets;
-            amtTax = subTotal * taxRate;
-            total = subTotal + amtTax;
+            if (widgetsWorked && rateValid)
+            {
+                // regular Processing
+                taxRate = taxRate / 100.0M;
 
-            //Output - every variable
-            lstOut.Items.Add("Customer Name: " + customerName);
-            lstOut.Items.Add("Widget Price: " + widgetPrice.ToString("C"));
-            lstOut.Items.Add("Number of Widgets Ordered: " + numWidgets.ToString("N0"));
-            lstOut.Items.Add("Tax Rate: " + taxRate.ToString("P"));
-            lstOut.Items.Add("Subtotal: " + subTotal.ToString("C"));
-            lstOut.Items.Add("Amount of Tax: " + amtTax.ToString("C"));
-            lstOut.Items.Add("Total: " + total.ToString("C"));
-          //  lstOut.Items.Add(5 / 3)
-            //lstOut.Items.Add(Math.Sqrt(16));
+                //Processing            
+                subTotal = widgetPrice * numWidgets;
+                amtTax = subTotal * taxRate;
+                total = subTotal + amtTax;
 
-            // this makes the reset button the active control (reset is given the focus)
-            btnReset.Focus();
+                //Output - every variable
+                lstOut.Items.Add("Customer Name: " + customerName);
+                lstOut.Items.Add("Widget Price: " + widgetPrice.ToString("C"));
+                lstOut.Items.Add("Number of Widgets Ordered: " + numWidgets.ToString("N0"));
+                lstOut.Items.Add("Tax Rate: " + taxRate.ToString("P"));
+                lstOut.Items.Add("Subtotal: " + subTotal.ToString("C"));
+                lstOut.Items.Add("Amount of Tax: " + amtTax.ToString("C"));
+                lstOut.Items.Add("Total: " + total.ToString("C"));
+                //  lstOut.Items.Add(5 / 3)
+                //lstOut.Items.Add(Math.Sqrt(16));
+
+                // this makes the reset button the active control (reset is given the focus)
+                btnReset.Focus();
+            } else
+            {
+               // lstOut.Items.Add("Input error");
+                if (!widgetsWorked)
+                {
+                    lstOut.Items.Add("Number of Widgets is not a whole number");
+                } 
+                if(!rateValid) 
+                {
+                    lstOut.Items.Add("Tax Rate is not a number");
+                }
+                
+            }
         }
 
         private void txtCustomerName_Enter(object sender, EventArgs e)
